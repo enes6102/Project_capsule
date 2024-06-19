da0f319e881af9c71db9210af697a307026354b5
Enes <abdullahenes6102@gmail.com>
2024-06-19 20:57:44 +0100

    options menu
    Added an options menu which has a level selector and will have more functionality later on.

--- Assets/scripts/StartMenu.cs
+++ Assets/scripts/StartMenu.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    //play game
     public void StartGame()
     {
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //quit game
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game");
     }
 }
